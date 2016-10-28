da438c9dbb210eab0c99d00d5dd5cf383069fcf5
Tarik Moataz <tarikmoataz@Tariks-MacBook-Pro.local>
2016-10-28 11:51:30 -0400

    Major updates: refer to the Extended Description

--- src/main/java/org/crypto/sse/SecureSetM.java
+++ src/main/java/org/crypto/sse/SecureSetM.java
@@ -1,3 +1,20 @@
+/** * Copyright (C) 2016 Tarik Moataz
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
 //***********************************************************************************************//
 // This file mainly includes the implementation of Matryoshka filter which is a component of the ZMF encrypted multi map. It makes use of Bloom Filters and Online Ciphers
 // In particular, we have the four algorithms for ZMF algorithn. This includes KeyGen, setup that builds of Matryoshka filters, a token algorithm and Test algorithm
--- src/test/java/org/crypto/sse/TestLocalZMF.java
+++ src/test/java/org/crypto/sse/TestLocalZMF.java
@@ -1,3 +1,20 @@
+/** * Copyright (C) 2016 Tarik Moataz
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
 /*
  * This class is a test for the ZMF construction. 
  * It tests for conjunctive queries where the client outputs two keywords that he wants to search for and the search will output the number of documents that contain 
